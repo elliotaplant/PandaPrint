7a0cb0e26cc09317a8e2eec77440565668fba2df
Elliot Plant <elliotaplant@gmail.com>
2018-03-07 15:51:00 -0800

    Add a couple more utils tests

--- src/utils/utils.spec.ts
+++ src/utils/utils.spec.ts
@@ -9,5 +9,18 @@ describe('Utils', () => {
       const gotten = Utils.safeGet(() => 5, null);
       expect(gotten).to.equal(5);
     });
+
+    it('should be able to access a deep property', () => {
+      const dinoTree = { big: { scary: 't-rex', friendly: 'brontosaurus' } };
+      const scaryDino = Utils.safeGet(() => dinoTree.big.scary, 'leoplaridon');
+      expect(scaryDino).to.equal(dinoTree.big.scary);
+    });
+
+    it('should handle errors coming from deep errors', () => {
+      const defaultSmallScaryDino = 'velociraptor';
+      const dinoTree: any = { big: { scary: 't-rex', friendly: 'brontosaurus' } };
+      const smallDino = Utils.safeGet(() => dinoTree.small.scary, defaultSmallScaryDino);
+      expect(smallDino).to.equal(defaultSmallScaryDino);
+    });
   });
 });
