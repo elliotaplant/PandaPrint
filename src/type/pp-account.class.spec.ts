e5c5f0f2f0cc8b7ec051bcc8201e0d0a0abc9586
Elliot Plant <elliotaplant@gmail.com>
2018-03-07 16:11:38 -0800

    Add tests for pp-account

---
+++ src/type/pp-account.class.spec.ts
@@ -0,0 +1,21 @@
+import { PpAccount } from './pp-account.class';
+import { OrderStatus } from '../type';
+import { expect } from 'chai';
+
+// Spec file for PpAccount
+describe('PpAccount', () => {
+
+  describe('constructor', () => {
+    let account: PpAccount;
+
+    beforeEach(() => {
+      account = new PpAccount()
+    });
+
+    it('should create an account with an empty current order and no previous orders', () => {
+      expect(account.currentOrder.pictureUrls).to.be.empty;
+      expect(account.currentOrder.status).to.equal(OrderStatus.Open);
+      expect(account.previousOrders).to.be.empty;
+    });
+  });
+});
